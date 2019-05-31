80c200a05dde0bfd152b52aab3f052cbcfc57e59
Meghna Srivastava <meghna.s1906@gmail.com>
2019-05-31 10:46:08 +0200

    added welcome page with sequential stories

---
+++ .storybook/welcomeStory.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+storiesOf('Welcome', module).add(
+    'to your new story book', () => (
+        <h1>Welcome to your new Storybook</h1>
+    ));
