c51ff43d440608e9c3509e9ab94f58e43e1f2f72
Meghna Srivastava <meghna.s1906@gmail.com>
2019-05-31 14:26:49 +0200

    add style to react storybook with info addon

--- src/Button.stories.js
+++ src/Button.stories.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import { withInfo } from '@storybook/addon-info';
 import { storiesOf } from '@storybook/react';
 
 import { Button } from './Button';
 
 storiesOf('Button', module)
-    .addWithJSX('with background 1', () => (<Button bg="limegreen">Hello world</Button>))
+    .addWithJSX(
+        'with background 1',
+        withInfo({
+            styles: {
+                header: {
+                    h1: {
+                        color: 'red',
+                    },
+                },
+            },
+            text: `
+            description
+            
+            ~~~js
+            <Button>dsdssfdffdg</Button>
+            ~~~
+            `,
+        })(() => <Button bg="limegreen">Hello world</Button>)
+    )
     .addWithJSX('with background 2', () => (<Button bg="lightblue">Hello world</Button>));
