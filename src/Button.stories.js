1650d1f8525e761318e01d4ba33d4ea8f4d0b2d2
Meghna Srivastava <meghna.s1906@gmail.com>
2019-05-31 11:10:37 +0200

    display jsx of react storybook story

--- src/Button.stories.js
+++ src/Button.stories.js
@@ -3,8 +3,6 @@ import { storiesOf } from '@storybook/react';
 
 import { Button } from './Button';
 
-storiesOf('Button', module).add('with background 1', () => (
-    <Button bg="limegreen">Hello world</Button>
-)).add('with background 2', () => (
-    <Button bg="lightblue">Hello world</Button>
-));
+storiesOf('Button', module)
+    .addWithJSX('with background 1', () => (<Button bg="limegreen">Hello world</Button>))
+    .addWithJSX('with background 2', () => (<Button bg="lightblue">Hello world</Button>));
